23f4e92e70269c6bec35aac594f0b51db006d718
Robert Pliszka <robplisz@gmail.com>
2023-02-04 21:08:07 -0500

    Added other page components needed for application

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
+      <div class="App">
+        {/* <!-- Add a SearchBar component --> */}
+        <div className="App-playlist">
+          {/* <!-- Add a SearchResults component -->
+      <!-- Add a Playlist component --> */}
+        </div>
+      </div>
     </div>
   );
 }
