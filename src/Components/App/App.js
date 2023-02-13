854e17345df548356014e4f85787bdec4ba4d1fc
Robert Pliszka <robplisz@gmail.com>
2023-02-13 18:47:08 -0500

    Added save playlist method to save the playlist. All tested works

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -56,6 +56,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   // Updates playlist name
@@ -88,6 +89,11 @@ class App extends React.Component {
     this.setState({ playlistTracks: tracks });
   }
 
+  savePlaylist() {
+    const trackUris = [this.state.playlistTracks.map((track) => track.uri)];
+    
+  }
+
   render() {
     return (
       <div>
@@ -106,6 +112,7 @@ class App extends React.Component {
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
               onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
             />
           </div>
         </div>
