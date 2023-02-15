c06c58f3c2f0834f85f7e2408dfe695ad9ba1dce
Robert Pliszka <robplisz@gmail.com>
2023-02-14 21:46:15 -0500

    Finished search to update the state

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -4,7 +4,13 @@ import "./SearchBar.css";
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      term: "",
+    };
+
     this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
   }
 
   // Search method
@@ -12,10 +18,19 @@ class SearchBar extends React.Component {
     // Passes the state of the term to the onSearch
     this.props.onSearch(this.state.term);
   }
+
+  // Handles term change
+  handleTermChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
   render() {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
+        <input
+          placeholder="Enter A Song, Album, or Artist"
+          onChange={this.handleTermChange}
+        />
         <button className="SearchButton">SEARCH</button>
       </div>
     );
