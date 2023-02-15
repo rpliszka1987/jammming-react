c35166f098a5c2c0f0384d0a6c8c50e91a4925e2
Robert Pliszka <robplisz@gmail.com>
2023-02-14 22:28:45 -0500

    Started setting up  Spotify API.

---
+++ src/util/Spotify.js
@@ -0,0 +1,29 @@
+const clientId = "32090f2979b943f1af5e845612d50115";
+const redirectUri = "http://localhost:3000/";
+let accessTocken;
+
+const Spotify = {
+  getAccessTocken() {
+    if (accessToken) {
+      return accessTocken;
+    }
+
+    // check for an access tocken match
+    // This gets the current url of our page
+    const accessTockenMatch =
+      window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    // Checks if valuesd exist in url
+    if (accessTockenMatch && expiresInMatch) {
+      accessTocken = accessTockenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+      // This clears the parameters, allowing us to grab new access tocken when it expires
+      window.setTimeout(() => (accessTocken = ""), expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/");
+      return accessToken;
+    }
+  },
+};
+
+export default Spotify;
