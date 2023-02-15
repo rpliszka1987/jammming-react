dbc2c741c39ad5262f005e4b2fa4948732c13b17
Robert Pliszka <robplisz@gmail.com>
2023-02-14 21:38:16 -0500

    Started linking together search bar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,6 +2,16 @@ import React from "react";
 import "./SearchBar.css";
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.search = this.search.bind(this);
+  }
+
+  // Search method
+  search() {
+    // Passes the state of the term to the onSearch
+    this.props.onSearch(this.state.term);
+  }
   render() {
     return (
       <div className="SearchBar">
